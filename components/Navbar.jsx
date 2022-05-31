4b6712157f838a158f8e1205db998c7a750a4ec5
Danhalf <danhel@rocketmail.com>
2022-05-31 22:56:41 +0300

    init&global styles

---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>12</li>
+      </ul>
+      <button className="button-primary">Click me</button>
+    </div>
+  );
+};
+
+export default Navbar;
